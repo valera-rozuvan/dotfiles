f5399baceb7eea2cc1fc7365ffe8d4a2d9b705c9
Valera Rozuvan <2c7v7czk85j3@protonmail.com>
2021-05-28 16:14:55 +0300

    bashrc stuff

---
+++ bash/.bash.rc/01-common.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+export LC_ALL="en_US.UTF-8"
+
+export LANG="en_US.UTF-8"
+export LC_COLLATE="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+export LC_MESSAGES="en_US.UTF-8"
+export LC_MONETARY="en_US.UTF-8"
+export LC_NUMERIC="en_US.UTF-8"
+export LC_TIME="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+
+export GPG_TTY=$(tty)
+
+export VISUAL="/bin/nano"
+export EDITOR="$VISUAL"
+export VIEWER="$VISUAL"
+
+export HISTIGNORE="pwd:df:du:exit:ls"
+export HISTSIZE=5000
+export HISTFILESIZE=20000
