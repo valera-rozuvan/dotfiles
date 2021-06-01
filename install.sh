aca866008c72e2027da39930f286af2dbe3b1977
Valera Rozuvan <2c7v7czk85j3@protonmail.com>
2021-06-01 12:54:25 +0300

    proper exec rights; update Urxvt settings

--- install.sh
+++ install.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+WD=$(pwd)
+
+chmod a-x $WD/.bashrc
+chmod -R a-x $WD/.bashrc.d
+
+chmod a-x $WD/.profile
+chmod -R a-x $WD/.profile.d
+
+chmod a-x $WD/.bash_logout
+chmod a-x $WD/.Xdefaults
+chmod a-x $WD/.xsessionrc
+
+chmod u+x $WD/.profile.d/01-ssh-agent.sh
+
 rm -rf ~/.bash*
 rm -rf ~/.profile*
 rm -rf ~/.nano*
 rm -rf ~/.Xdefaults*
-rm -rf ~/.xsessionrc*
-
-WD=$(pwd)
+rm -rf ~/.xsession*
 
 ln -s $WD/.bashrc.d    ~/.bashrc.d
 ln -s $WD/.profile.d   ~/.profile.d
