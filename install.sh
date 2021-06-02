0d490951378740bcf1dca4829eea570621f7c243
Valera Rozuvan <2c7v7czk85j3@protonmail.com>
2021-06-02 22:29:56 +0300

    better error handling

--- install.sh
+++ install.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+set -o errexit
+set -o pipefail
+
+function hndl_SIGHUP() {
+  echo "Unfortunately, the script received SIGHUP..."
+  exit 1
+}
+function hndl_SIGINT() {
+  echo "Unfortunately, the script received SIGINT..."
+  exit 1
+}
+function hndl_SIGQUIT() {
+  echo "Unfortunately, the script received SIGQUIT..."
+  exit 1
+}
+function hndl_SIGABRT() {
+  echo "Unfortunately, the script received SIGABRT..."
+  exit 1
+}
+function hndl_SIGTERM() {
+  echo "Unfortunately, the script received SIGTERM..."
+  exit 1
+}
+
+trap hndl_SIGHUP  SIGHUP
+trap hndl_SIGINT  SIGINT
+trap hndl_SIGQUIT SIGQUIT
+trap hndl_SIGABRT SIGABRT
+trap hndl_SIGTERM SIGTERM
+
 WD=$(pwd)
 
 chmod u+x $WD/.profile.d/01-ssh-agent.sh
@@ -19,5 +49,5 @@ ln -s $WD/.profile     ~/.profile
 ln -s $WD/.Xdefaults   ~/.Xdefaults
 ln -s $WD/.xsessionrc  ~/.xsessionrc
 
-echo "Done!"
+echo -e "\nDone, without errors ;)"
 exit 0
