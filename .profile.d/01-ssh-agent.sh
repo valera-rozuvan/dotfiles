4f5bf5a81ae7eaee5d5d22c218e38214b4f80b57
Valera Rozuvan <2c7v7czk85j3@protonmail.com>
2021-06-03 13:47:47 +0300

    Make SSH_ENV available everywhere. Part 3.

--- .profile.d/01-ssh-agent.sh
+++ .profile.d/01-ssh-agent.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SSH_ENV="$HOME/.ssh/agent-environment"
+
 function start_agent {
     echo "Initialising new SSH agent..."
     /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
